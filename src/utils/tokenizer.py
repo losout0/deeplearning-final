2682182a5bbffb24f3c52873540c6df319fa5c05
Leandro <leandro.oliveira.souto@ccc.ufcg.edu.br>
2025-09-21 10:00:31 -0300

    refactor: definindo device padrão

--- src/utils/tokenizer.py
+++ src/utils/tokenizer.py
@@ -3,12 +3,12 @@ import tiktoken
 
 tokenizer = tiktoken.get_encoding("o200k_base")
 
-def text_to_token_ids(text, tokenizer, device):
+def text_to_token_ids(text, tokenizer, device="cpu"):
     encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
     encoded_tensor = torch.tensor(encoded).unsqueeze(0) # Adiciona o Batch
     return encoded_tensor.to(device)
 
 
-def token_ids_to_text(token_ids, tokenizer, device):
+def token_ids_to_text(token_ids, tokenizer, device="cpu"):
     flat = token_ids.squeeze(0) # Tira a dimensão de Batch
     return tokenizer.decode(flat.tolist())
